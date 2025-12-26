3f21809d3c79c477bc244b7e1604f79ddd6805c9
“Pinto” <lagadorj@gmail.com>
2025-12-26 09:03:08 -0300

    Commit com arquivos básicos do projeto

---
+++ GameLibrary_SpringBoot/src/main/java/com/senac/GameLibrary_SpringBoot/GameLibrarySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.senac.GameLibrary_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibrarySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibrarySpringBootApplication.class, args);
+	}
+
+}
